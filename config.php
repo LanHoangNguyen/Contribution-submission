592fafd6b69d4aaee6ad41a7f742446fbe43b11a
LanHoangNguyen <55873544+LanHoangNguyen@users.noreply.github.com>
2021-04-04 10:11:17 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 $db = parse_url( getenv("DATABASE_URL") );
-$host = $db["host"]
-$port = "5433";
-$dbname = "postgres";
-$user = "postgres";
-$password = "postgres"; 
+$host = $db["host"];
+$port = $db["port"];
+$dbname = ltrim($db["path"], "/");
+$user = $db["user"];
+$password = $db["pass"]"; 
 $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
 $dbconn = pg_connect($connection_string);
 ?>
