426d23f2396dff2d38afc60aef356e15e694f436
LanHoangNguyen <55873544+LanHoangNguyen@users.noreply.github.com>
2021-04-04 09:39:32 +0700

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,19 @@
+<?php
+include('header.php');
+include('session.php');
+include('config.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="./bootstrap-5.0.0-beta2-dist/css/bootstrap.css">
+    <script src="./bootstrap-5.0.0-beta2-dist/js/bootstrap.js"></script>
+</head>
+<body>
+	<a href="accountmanagement.php">Create new accounts</a>
+	<a href="closuredate.php">Set and edit closure date</a>
+</body>
+</html>
---
+++ index.html
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Welcome to the contribution submission page</h1>
+	<a href="login.php">Click here to login</a>
+</body>
+</html>
